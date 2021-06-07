f1bf3087bd1098d5d088d09d3a7203db86b090c3
Venomous2010 <79148708+Venomous2010@users.noreply.github.com>
2021-06-07 18:06:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var names = [
+
+]
+
+function submit() {
+    names.push(input1);
+    names.push(input2);
+    names.push(input3);
+    names.push(input4);
+    names.push(input5);
+    document.getElementById().innerHTML = names;
+    document.getElementById("submit").style.display = "none";
+    document.getElementById("sort").style.display = "inline-block";
+}
